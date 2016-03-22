ce1be262c84e3ca2c140b65d75466012dd4f7534
LG <kks3805@naver.com>
2016-03-22 14:10:20 +0900

    fragmentTabhost + tabwidget.index(0) clicked적용 취소!!!! 일반적인 fragmentTabhost를 사용한다.

--- app/src/main/java/com/fitta/lightsoo/fitta/MainActivity.java
+++ app/src/main/java/com/fitta/lightsoo/fitta/MainActivity.java
@@ -1,13 +1,10 @@
 package com.fitta.lightsoo.fitta;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TabWidget;
-import android.widget.Toast;
 
 import com.fitta.lightsoo.fitta.Fragment.FittingFragment;
 import com.fitta.lightsoo.fitta.Fragment.FittingRoomFragment;
@@ -16,7 +13,8 @@ import com.fitta.lightsoo.fitta.Fragment.SettingFragment;
 public class MainActivity extends AppCompatActivity {
 
     private FragmentTabHost tabHost;
-    private TabWidget tabWidget;
+    //드로우 메뉴를 하기위한 clicked리스너였는데 일단은 제외한다.
+//    private TabWidget tabWidget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,42 +26,40 @@ public class MainActivity extends AppCompatActivity {
         //false해서 기존 title을 없애
         getSupportActionBar().setDisplayShowTitleEnabled(false);*/
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         init();
     }
 
     public void init(){
         tabHost = (FragmentTabHost)findViewById(R.id.tabhost);
         tabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
-        tabWidget = (TabWidget)tabHost.getTabWidget();
-        /**
-         * 프레그먼트 두개를 넣어서 1,2번을 하고
-         * 1번 눌럿을때 드로우메뉴도 나오게한다.
-         */
+//        tabWidget = (TabWidget)tabHost.getTabWidget();
 
-        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("메뉴", null),
-                FittingFragment.class, null);
-        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("피팅", null),
-                FittingFragment.class, null);
-        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("피팅룸", null),
-                FittingRoomFragment.class, null);
-        tabHost.addTab(tabHost.newTabSpec("tab4").setIndicator("세팅", null),
-                SettingFragment.class, null);
-        tabHost.setCurrentTab(1);
 
-        View tabwidgetgetview = (View)tabWidget.getChildTabViewAt(0);
+        View home = LayoutInflater.from(MainActivity.this).inflate(R.layout.tab_home_btn, null);
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator(home), FittingFragment.class, null);
+
+        View chat = LayoutInflater.from(MainActivity.this).inflate(R.layout.tab_chat_btn, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator(chat), FittingFragment.class, null);
+
+        View myinfo = LayoutInflater.from(MainActivity.this).inflate(R.layout.tab_myinfo_btn, null);
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator(myinfo), FittingRoomFragment.class, null);
+
+        View write = LayoutInflater.from(MainActivity.this).inflate(R.layout.tab_write_btn, null);
+        tabHost.addTab(tabHost.newTabSpec("tab4").setIndicator(write), SettingFragment.class, null);
+
+        tabHost.setCurrentTab(0);
+
+
+        //클릭리스너야
+       /* View tabwidgetgetview = (View)tabWidget.getChildTabViewAt(0);
         tabwidgetgetview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "cliked", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
+
+
     }
 }
