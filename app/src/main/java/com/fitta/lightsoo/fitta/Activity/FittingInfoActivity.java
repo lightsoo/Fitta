c715fe4d07473d3afe02b735415f42b9a57af67d
LG <kks3805@naver.com>
2016-03-28 17:45:14 +0900

    피팅프레그먼트 플로우 UI설정중 FittingFragment에서 FittingInfoActivity로 flag전달 끝

--- app/src/main/java/com/fitta/lightsoo/fitta/Activity/FittingInfoActivity.java
+++ app/src/main/java/com/fitta/lightsoo/fitta/Activity/FittingInfoActivity.java
@@ -1,7 +1,11 @@
 package com.fitta.lightsoo.fitta.Activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
@@ -16,10 +20,16 @@ import android.widget.Toast;
 
 import com.fitta.lightsoo.fitta.R;
 
+import java.io.File;
+
 public class FittingInfoActivity extends AppCompatActivity {
+
+
+
 
     private static final int REQUEST_CAMERA = 100;
     private static final int REQUEST_GALLERY = 101;
+    private static final int REQUEST_CROP = 102;
     private String resultSize1 = "";
     private String resultSize2 = "";
     private Spinner spinner;
@@ -27,6 +37,15 @@ public class FittingInfoActivity extends AppCompatActivity {
     private EditText editSize ;
     private Button btn_post ;
     private static int flag=0;
+
+
+
+    private File mSavedFile;
+
+    //카메라를 찍은 다음 사진을 임시로 저장해서 이후에 크롭 인텐트를 이용해서
+    // THEMP_PHOTO_FILE로 명명해서 크롭된 이미지를 사용
+    private static final String TEMP_CAMERA_FILE = "temp_camera.jpg";
+    private static final String TEMP_PHOTO_FILE = "temp_album.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,7 +96,11 @@ public class FittingInfoActivity extends AppCompatActivity {
                 if(!preInspection()){
                     Toast.makeText(FittingInfoActivity.this, "빈칸있다!", Toast.LENGTH_SHORT).show();
                 }else{//여기서 액티비티 flag에 따라 카메라, 갤러리액티비티로 이동한다.
+                    if(flag == REQUEST_CAMERA){
 
+                    }else if(flag == REQUEST_GALLERY){
+                        getGalleryImage();
+                    }
                 }
             }
         });
@@ -109,6 +132,26 @@ public class FittingInfoActivity extends AppCompatActivity {
 
         Log.d("test", resultSize2);
     }
+
+
+    private void getGalleryImage(){
+        Intent photoPickerIntent = new Intent(
+                Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        photoPickerIntent.setType("image/*");
+        photoPickerIntent.putExtra("crop", "true");
+
+        photoPickerIntent.putExtra(MediaStore.EXTRA_OUTPUT, getTempUri());
+        photoPickerIntent.putExtra("outputFormat",
+                Bitmap.CompressFormat.JPEG.toString());
+        startActivityForResult(photoPickerIntent, REQUEST_GALLERY);
+    }
+
+    private Uri getTempUri() {
+        mSavedFile = new File(Environment.getExternalStorageDirectory(), TEMP_PHOTO_FILE);
+        return Uri.fromFile(mSavedFile);
+//        return Uri.fromFile(getTempFile());
+    }
+
 
    /* @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
--- app/src/main/java/com/fitta/lightsoo/fitta/Activity/FittingResultActivity.java
+++ app/src/main/java/com/fitta/lightsoo/fitta/Activity/FittingResultActivity.java
@@ -1,15 +1,21 @@
 package com.fitta.lightsoo.fitta.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.fitta.lightsoo.fitta.Camera.CameraActivity;
 import com.fitta.lightsoo.fitta.R;
 
 public class FittingResultActivity extends AppCompatActivity {
     private ImageView avatar;
+    private static final int REQ_CAMERA_IMAGE = 123;
+
+    private static final String TAG = "FittingResultActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,4 +37,24 @@ public class FittingResultActivity extends AppCompatActivity {
                 .into(avatar);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //카메라 촬영이후 이미지의 경로를 준다,
+        if (requestCode == REQ_CAMERA_IMAGE && resultCode == RESULT_OK) {
+            //CameraActivity의 onPictureTaken()에서 경로 입력 받아.
+            String imgPath = data.getStringExtra(CameraActivity.EXTRA_IMAGE_PATH);
+            Log.i(TAG,"Got image path: " + imgPath);
+
+            //이미지를 앨범에 저장하는게 필요.
+//            saveImage(imgPath, FittingResultActivity.this);
+            //이미지뷰에 설정해서 출력해주는데
+//            displayImage(imgPath);
+
+        } else if (requestCode == REQ_CAMERA_IMAGE && resultCode == RESULT_CANCELED) {
+            Log.i(TAG,"User didn't take an image");
+        }
+
+    }
 }
